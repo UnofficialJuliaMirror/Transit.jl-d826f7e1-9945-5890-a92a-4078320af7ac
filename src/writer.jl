891de04223eb4ade5ec427a18a701772a420210a
Russ Olsen <russ@russolsen.com>
2016-05-10 13:20:32 -0400

    Add verbose option to to_transit.

--- src/writer.jl
+++ src/writer.jl
@@ -3,9 +3,10 @@ function write(io::IO, x::Any, verbose=false)
     encode_top_level(e, x)
 end
 
-function to_transit(x::Any)
+function to_transit(x::Any, verbose=false)
     let buf = IOBuffer()
-        write(buf, x)
-        takebuf_string(buf)
+        write(buf, x, verbose)
+        s = takebuf_string(buf)
+	s
     end
 end
