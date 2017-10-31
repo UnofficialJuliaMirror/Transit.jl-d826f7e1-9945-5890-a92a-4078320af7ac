29a724a2d23b5b0a070d1912e8f3e7e13d8352cd
Ben Kamphaus <ben.kamphaus@gmail.com>
2017-10-31 15:52:23 -0600

    Clean up top module so that indenting format matches typical Julia style and exports include parse, write only

--- src/Transit.jl
+++ src/Transit.jl
@@ -1,25 +1,27 @@
 module Transit
-  import JSON
-  import Base.getindex
-  import Base
 
-  using DataStructures
-  using URIParser
-  using Decimals
+import JSON
+import Base.getindex
+import Base
 
-  export Encoder, encode, parse, decode
+using DataStructures
+using URIParser
+using Decimals
 
-  include("tagged_value.jl")
-  include("tsymbol.jl")
-  include("tset.jl")
-  include("turi.jl")
-  include("link.jl")
-  include("constants.jl")
-  include("cache.jl")
-  include("decoder.jl")
-  include("utilities.jl")
-  include("emitter.jl")
-  include("encoder.jl")
-  include("writer.jl")
-  include("reader.jl")
+export parse, write
+
+include("tagged_value.jl")
+include("tsymbol.jl")
+include("tset.jl")
+include("turi.jl")
+include("link.jl")
+include("constants.jl")
+include("cache.jl")
+include("decoder.jl")
+include("utilities.jl")
+include("emitter.jl")
+include("encoder.jl")
+include("writer.jl")
+include("reader.jl")
+
 end
